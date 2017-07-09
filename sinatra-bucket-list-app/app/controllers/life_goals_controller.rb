5312fe07e610315d8ff09cf499bacdaadacfb4d6
machen2 <victoriamachen@gmail.com>
2017-07-09 03:15:36 +0000

    added very basic new and post life goals controller and views

--- sinatra-bucket-list-app/app/controllers/life_goals_controller.rb
+++ sinatra-bucket-list-app/app/controllers/life_goals_controller.rb
@@ -5,5 +5,17 @@ class LifeGoalsController < ApplicationController
     erb :'/life_goals/index'
   end
 
+  get '/life_goals/new' do
+    erb :'/life_goals/new'
+  end
 
+  get '/life_goals/:id' do #show page
+    @life_goal = LifeGoal.find_by(id: params[:id])
+    erb :'/life_goals/show'
+  end
+
+  post '/life_goals' do
+    @life_goal = LifeGoal.new(params[:life_goal])
+    erb :"/life_goals/#{@life_goal.id}"
+  end
 end
