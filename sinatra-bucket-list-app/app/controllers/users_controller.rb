7a99f1731cc50685a9f041ea0d71257915d0bedc
machen2 <victoriamachen@gmail.com>
2017-07-10 16:41:40 +0000

    started basic signup action in the users controller

--- sinatra-bucket-list-app/app/controllers/users_controller.rb
+++ sinatra-bucket-list-app/app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    erb :signup
+  end
 
+  post '/signup' do
+    @user = User.new(name: params[:name], username: params[:username], password: params[:password], email: params[:email])
+    @user.save
+    session[:user_id] = @user.id
+    redirect to '/life_goals'
+  end
 end
--- sinatra-bucket-list-app/app/models/users.rb
+++ sinatra-bucket-list-app/app/models/users.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :life_goals
-end 
+
+  has_secure_password
+end
