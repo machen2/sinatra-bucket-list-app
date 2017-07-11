ec0c77eee3ef42f3671a65cb8595f6b27c5754f8
machen2 <victoriamachen@gmail.com>
2017-07-11 03:49:10 +0000

    added logged_in? varifications to life_goals controller show and index actions

--- sinatra-bucket-list-app/app/controllers/life_goals_controller.rb
+++ sinatra-bucket-list-app/app/controllers/life_goals_controller.rb
@@ -1,12 +1,20 @@
 class LifeGoalsController < ApplicationController
 
   get '/life_goals' do #index page
-    @life_goals = current_user.life_goals
-    erb :'/life_goals/index'
+    if logged_in?
+      @life_goals = current_user.life_goals
+      erb :'/life_goals/index'
+    else
+      redirect to '/'
+    end
   end
 
   get '/life_goals/new' do
-    erb :'/life_goals/new'
+    if logged_in?
+      erb :'/life_goals/new'
+    else
+      redirect to '/'
+    end
   end
 
   post '/life_goals' do
@@ -18,7 +26,11 @@ class LifeGoalsController < ApplicationController
 
   get '/life_goals/:id' do #show page
     @life_goal = LifeGoal.find_by(id: params[:id])
-    erb :'/life_goals/show'
+    if logged_in? && (current_user.id == @life_goal.user_id)
+      erb :'/life_goals/show'
+    else
+      redirect to '/life_goals'
+    end
   end
 
   get '/life_goals/:id/edit' do #edit page
